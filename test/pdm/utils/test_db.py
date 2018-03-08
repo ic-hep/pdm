d2fb97bdb29a2308ab80e98328a2b4a4da64cd1b
Simon Fayer <sf105@ic.ac.uk>
2018-03-08 19:31:19 +0000

    Add minimal tests for new managed_session features

--- test/pdm/utils/test_db.py
+++ test/pdm/utils/test_db.py
@@ -9,22 +9,30 @@ from pdm.utils.db import managed_session
 class TestDBUtils(unittest.TestCase):
   """ Test all DB util functions. """
 
-  def run_db_session(self, test_db):
-      with managed_session(test_db) as db_session:
-          self.assertEquals(db_session, test_db.session)
+  def run_db_session(self, test_request, **kwargs):
+      with managed_session(test_request, **kwargs) as db_session:
+          self.assertEquals(db_session, test_request.db.session)
 
   def test_managed_session(self):
     """ Test managed_session generator. """
-    test_db = mock.MagicMock()
-    test_db.session = mock.MagicMock()
+    test_request = mock.MagicMock()
+    test_request.db = mock.MagicMock()
+    test_request.db.session = mock.MagicMock()
     # Check basic commit
-    self.run_db_session(test_db)
-    self.assertTrue(test_db.session.commit.called)
-    self.assertFalse(test_db.session.rollback.called)
-    test_db.session.commit.called = False # Reset
+    self.run_db_session(test_request)
+    self.assertTrue(test_request.db.session.commit.called)
+    self.assertFalse(test_request.db.session.rollback.called)
+    test_request.db.session.commit.called = False # Reset
     # Check error case
-    test_db.session.commit.side_effect = Exception("DB Error")
-    self.assertRaises(Exception, self.run_db_session, test_db)
-    self.assertTrue(test_db.session.commit.called)
-    self.assertTrue(test_db.session.rollback.called)
-    
+    test_request.db.session.commit.side_effect = Exception("DB Error")
+    self.assertRaises(Exception, self.run_db_session, test_request)
+    self.assertTrue(test_request.db.session.commit.called)
+    self.assertTrue(test_request.db.session.rollback.called)
+    test_request.db.session.commit.called = False
+    test_request.db.session.rollback.called = False
+    # Check abort error case
+    from werkzeug.exceptions import InternalServerError
+    self.assertRaises(InternalServerError, self.run_db_session,
+                      test_request, http_error_code=500)
+    self.assertTrue(test_request.db.session.commit.called)
+    self.assertTrue(test_request.db.session.rollback.called)
