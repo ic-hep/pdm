619821bf207cbee70b3366a424949902c2a28386
Alexander Richards <a.richards@imperial.ac.uk>
2018-03-14 14:15:39 +0000

    Fixes from testing.

--- src/pdm/workqueue/WorkqueueService.py
+++ src/pdm/workqueue/WorkqueueService.py
@@ -175,8 +175,8 @@ class WorkqueueService(object):
                        .filter(Job.status.in_((JobStatus.DONE, JobStatus.FAILED)))\
                        .first_or_404()
         dir_ = os.path.join(getConfig("app/workqueue").get('workerlogs', '/tmp/workers'),
-                            job.log.guid[:2],
-                            job.log.guid)
+                            job.log_uid[:2],
+                            job.log_uid)
         with open(os.path.join(dir_, "attempt%i.log" % job.attempts, 'rb')) as logfile:
             log = logfile.read()
 
