45e5c73dce570758d1361dd244c7f092c36b960d
Janusz Martyniak <janusz.martyniak@googlemail.com>
2018-03-14 16:59:28 +0000

    cosmetics and some docs added

--- src/pdm/userservicedesk/TransferClientFacade.py
+++ src/pdm/userservicedesk/TransferClientFacade.py
@@ -1,5 +1,8 @@
-from pdm.userservicedesk.TransferClient import TransferClient
+""" The Facade for the TransferClient. Uses URL as an argument to most
+    of the methods (rather them its parts)
+"""
 from urlparse import urlparse
+from pdm.userservicedesk.TransferClient import TransferClient
 
 
 class TransferClientFacade(TransferClient):
@@ -11,19 +14,40 @@ class TransferClientFacade(TransferClient):
         super(TransferClientFacade, self).__init__(token)
 
     def list(self, url, **kwargs):
+        """
+        List the resource specified by URL.
+        :param url: URL of the resource
+        :param kwargs: additional arguments (like max_tries or priority)
+        :return: the resource listing
+        """
         parts = urlparse(url)
         if parts.scheme:
             kwargs['protocol'] = parts.scheme
-        return super(TransferClientFacade, self).list(parts.netloc, parts.path, **kwargs )
+        return super(TransferClientFacade, self).list(parts.netloc, parts.path, **kwargs)
 
     def copy(self, src_url, dst_url, **kwargs):
+        """
+        Copy files or directories from one site to another.
+        :param src_url: source resource URL
+        :param dst_url: destination resource URL
+        :param kwargs: addition arguments (like max_tries ot priority)
+        :return: copy result message
+        """
         dst_parts = urlparse(dst_url)
         src_parts = urlparse(src_url)
         if src_parts.scheme:
             kwargs['protocol'] = src_parts.scheme
-        super(TransferClientFacade, self).copy(src_parts.netloc, src_parts.path, dst_parts.netloc, dst_parts.path,
+        super(TransferClientFacade, self).copy(src_parts.netloc, src_parts.path,
+                                               dst_parts.netloc, dst_parts.path,
                                                **kwargs)
+
     def remove(self, src_url, **kwargs):
+        """
+        Remove the resources (file or directory)
+        :param src_url: URL of the resource
+        :param kwargs: additional arguments (like max_tries or priority)
+        :return: remove result message
+        """
         src_parts = urlparse(src_url)
         if src_parts.scheme:
             kwargs['protocol'] = src_parts.scheme
