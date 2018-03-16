daaa01f7071cad6cf218821d5bef075ad6087dfb
Daniela Bauer <daniela.bauer.grid@googlemail.com>
2018-03-16 15:55:08 +0000

    first steps towards file listing

--- src/pdm/web/WebPageService.py
+++ src/pdm/web/WebPageService.py
@@ -5,6 +5,7 @@ import flask
 from flask import request, flash
 from pdm.framework.FlaskWrapper import export, export_ext, startup, db_model, jsonify
 from pdm.userservicedesk.HRClient import HRClient
+from pdm.endpoint.EndpointClient import EndpointClient
 
 @export_ext("/web")
 class WebPageService(object):
@@ -23,6 +24,19 @@ class WebPageService(object):
         log = flask.current_app.log
         log.info("Hello Real Turtles")
         flask.current_app.hrclient = HRClient()
+    
+        flask.current_app.epclient = EndpointClient()
+
+    # checks if user is logged in    
+    @staticmethod
+    def check_session():
+        if "token" in flask.session:
+            # TODO: check if token is still valid
+            return
+        # TODO: Flash message about not being logged in
+        flask.abort(flask.redirect("/web/datamover"))    
+
+
 
     @staticmethod
     @export_ext("/")
@@ -31,10 +45,49 @@ class WebPageService(object):
         return flask.redirect("/web/datamover")
 
     @staticmethod
-    @export_ext("/web/datamover")
+    @export_ext("datamover")
     def website():
         status = "In Development"
         return flask.render_template("datamover.html", status=status)
+    
+    @staticmethod
+    @export_ext("datamover", methods=["POST"])
+    def website_post():
+        # check if login is correct
+        username = request.form['uname']
+        password = request.form['pswd']
+        try:
+            token = flask.current_app.hrclient.login(username, password)
+            flask.session["token"] = token
+        except Exception as err:
+            flash('Could not login user (%s)' % err)
+            return flask.render_template("datamover.html")
+
+        return flask.redirect("/web/dashboard")    
+
+    @staticmethod
+    @export_ext("logout")
+    def logout():
+        flask.session.pop("token")
+        flash('You have been logged out.')
+        return flask.redirect("/web/datamover")
+
+    @staticmethod
+    @export_ext("dashboard")
+    def dashboard():
+        # will abort of user is not logged in
+        WebPageService.check_session()
+        return flask.render_template("dashboard.html")
+
+    @staticmethod
+    @export_ext("listings", methods=["GET", "POST"])
+    def listings():
+        WebPageService.check_session()
+        sites = flask.current_app.epclient.get_sites()
+        return str(sites)
+        # return flask.render_template("listings.html")
+
+
 
     @staticmethod
     @export_ext("about")
