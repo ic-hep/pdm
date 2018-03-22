23f89fda8fb7870d46dab759357c794f3eb4d8ff
Simon Fayer <sf105@ic.ac.uk>
2018-03-22 17:00:13 +0000

    Enable checksum verification on copies by default

--- src/pdm/workqueue/scripts/copy.sh
+++ src/pdm/workqueue/scripts/copy.sh
@@ -2,4 +2,4 @@
 
 source /cvmfs/grid.cern.ch/umd-c7ui-latest/etc/profile.d/setup-c7-ui-example.sh
 
-gfal-copy -v "${SRC_PATH}" "${DST_PATH}"
+gfal-copy -vv --checksum-mode both "${SRC_PATH}" "${DST_PATH}"
