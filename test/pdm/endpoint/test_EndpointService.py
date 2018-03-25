3f85fe945a93b120a37c361fe46cd9d653a11d9e
Simon Fayer <sf105@ic.ac.uk>
2018-03-25 15:55:46 +0100

    Update tests to contain all valid endpoint URIs

--- test/pdm/endpoint/test_EndpointService.py
+++ test/pdm/endpoint/test_EndpointService.py
@@ -154,7 +154,7 @@ class test_EndpointService(unittest.TestCase):
         """ Test adding endpoints to a site and getting them with the
             main site info.
         """
-        TEST_URI = "gsiftp://test_host/test"
+        TEST_URI = "gsiftp://testhost/test"
         data = {'ep_uri': TEST_URI}
         # Try adding another endpoint to site 10.
         res = self.__client.post('endpoint/api/v1.0/site/10', data=data)
@@ -198,7 +198,7 @@ class test_EndpointService(unittest.TestCase):
     def test_add_endpoint_dberror(self, mock_session):
         """ Check that HTTP 500 is returned on DB errors. """
         self.__db_error(mock_session)
-        TEST_URI = "gsiftp://test_host/test"
+        TEST_URI = "gsiftp://testhost/test"
         data = {'ep_uri': TEST_URI}
         res = self.__client.post('endpoint/api/v1.0/site/1', data=data)
         self.assertEqual(res.status_code, 500)
