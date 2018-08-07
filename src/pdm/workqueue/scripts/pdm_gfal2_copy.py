8081e838a00add106f98319a6981a7f9a20ddf31
Janusz Martyniak <janusz.martyniak@googlemail.com>
2018-08-07 15:02:31 +0100

    leave the timeout default unchanged, if not given

--- src/pdm/workqueue/scripts/pdm_gfal2_copy.py
+++ src/pdm/workqueue/scripts/pdm_gfal2_copy.py
@@ -43,7 +43,7 @@ def monitor_callback(src, dst, average, instant, transferred, elapsed):  # pylin
 
 def pdm_gfal_copy(copy_dict, s_cred_file=None, t_cred_file=None, overwrite=False,
                   # pylint: disable=too-many-arguments, too-many-locals
-                  parent=True, nbstreams=1, timeout=300,
+                  parent=True, nbstreams=1, timeout=None,
                   verbosity=logging.INFO):
     """
     Copy a single source file to a target file.
@@ -82,7 +82,8 @@ def pdm_gfal_copy(copy_dict, s_cred_file=None, t_cred_file=None, overwrite=False
     params.overwrite = overwrite
     params.create_parent = parent
     params.nbstreams = nbstreams
-    params.timeout = timeout
+    if timeout is not None:
+        params.timeout = timeout
     params.event_callback = event_callback
     params.monitor_callback = monitor_callback
 
